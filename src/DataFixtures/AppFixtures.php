c18f3f28ef94d757120c59fc94839da158c4d5bc
Lord-forgos <johan.rousseau39@gmail.com>
2024-03-06 11:44:05 +0100

    reprise johan

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -27,6 +27,7 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+
           $publicUser = new User();
         $publicUser->setUsername("public");
         $publicUser->setRoles(["PUBLIC"]);
@@ -59,74 +60,97 @@ class AppFixtures extends Fixture
 
         $pokedexNames = ["MecaZouzoumon","Shmiromon","Chaussure","Anemon","Veylsimon","Kiffounmon", "ZouzouMon", "Kripipomon", "Chimpokomon", "Ramon", "Croketasderamon"];
 
-
-        for($i = 1; $i < 152;$i++){
-
-            $pokedex = new Pokedex();
-            // $pokedex->setName($this->faker->name());
-            $pokedex->setName($pokedexNames[array_rand($pokedexNames)]);
-            //Set PV
-            $pvMax = random_int($min,$max);
-            $pvMin = random_int($min,$pvMax);
-            $pokedex->setPvMin($pvMin);
-            $pokedex->setPvMax($pvMax);
-            
-            $attackMax = random_int($min,$max);
-            $attackMin = random_int($min,$attackMax);
-            $pokedex->setAttackMin($attackMin);
-            $pokedex->setAttackMax($attackMax);
-
-            $defenseMax = random_int($min,$max);
-            $defenseMin = random_int($min,$defenseMax);
-            $pokedex->setDefenseMin($defenseMin);
-            $pokedex->setDefenseMax($defenseMax);
-
-            $speedMax = random_int($min,$max);
-            $speedMin = random_int($min,$speedMax);
-            $pokedex->setSpeedMin($speedMin);
-            $pokedex->setSpeedMax($speedMax);
-
-            $specialMax = random_int($min ,$max);
-            $specialMin = random_int($min ,$specialMax);
-            $pokedex->setSpecialMin($specialMin);
-            $pokedex->setSpecialMax($specialMax);
-
-
-            $pokedex->setPokemonIdFirst(random_int(1,151));
-            $pokedex->setPokemonIdSecond(random_int(1,151));
-            $pokedexs[] = $pokedex;
-        }
-
-        foreach($pokedexs as $pokedex){
-            if(random_int(0,1)){
-                $pokedex->addDevolutionId($pokedexs[array_rand($pokedexs, 1)]);
-            }
-            $manager->persist($pokedex);
-        }
-        
-        $pokemons = [];
-        for($i = 1; $i < 3;$i++){
-            $pokemon = new Pokemon();
-            $pokedexRef = $pokedexs[array_rand($pokedexs, 1)];
-            $pokemon->setName($pokedexRef->getName());
-            $pvMax =rand($pokedexRef->getPvMin(),$pokedexRef->getPvMax());
-            $pokemon->setPvMax($pvMax);
-            $pokemon->setPv(rand(0, $pvMax));
-            $pokemon->setLevel(rand(0, 100));
-            
-            $pokemon->setPokedex($pokedexRef);
-            $pokemons[] = $pokemon;
-        }
-
-        foreach ($pokemons as $pokemon) {
-           $manager->persist($pokemon);
-        }
+//
+//        for($i = 1; $i < 152;$i++){
+//
+//            $pokedex = new Pokedex();
+//            // $pokedex->setName($this->faker->name());
+//            $pokedex->setName($pokedexNames[array_rand($pokedexNames)]);
+//            //Set PV
+//            $pvMax = random_int($min,$max);
+//            $pvMin = random_int($min,$pvMax);
+//            $pokedex->setPvMin($pvMin);
+//            $pokedex->setPvMax($pvMax);
+//
+//            $attackMax = random_int($min,$max);
+//            $attackMin = random_int($min,$attackMax);
+//            $pokedex->setAttackMin($attackMin);
+//            $pokedex->setAttackMax($attackMax);
+//
+//            $defenseMax = random_int($min,$max);
+//            $defenseMin = random_int($min,$defenseMax);
+//            $pokedex->setDefenseMin($defenseMin);
+//            $pokedex->setDefenseMax($defenseMax);
+//
+//            $speedMax = random_int($min,$max);
+//            $speedMin = random_int($min,$speedMax);
+//            $pokedex->setSpeedMin($speedMin);
+//            $pokedex->setSpeedMax($speedMax);
+//
+//            $specialMax = random_int($min ,$max);
+//            $specialMin = random_int($min ,$specialMax);
+//            $pokedex->setSpecialMin($specialMin);
+//            $pokedex->setSpecialMax($specialMax);
+//
+//
+//            $pokedex->setPokemonIdFirst(random_int(1,151));
+//            $pokedex->setPokemonIdSecond(random_int(1,151));
+//            $pokedexs[] = $pokedex;
+//        }
+//
+//        foreach($pokedexs as $pokedex){
+//            if(random_int(0,1)){
+//                $pokedex->addDevolutionId($pokedexs[array_rand($pokedexs, 1)]);
+//            }
+//            $manager->persist($pokedex);
+//        }
+//
+//        $pokemons = [];
+//        for($i = 1; $i < 3;$i++){
+//            $pokemon = new Pokemon();
+//            $pokedexRef = $pokedexs[array_rand($pokedexs, 1)];
+//            $pokemon->setName($pokedexRef->getName());
+//            $pvMax =rand($pokedexRef->getPvMin(),$pokedexRef->getPvMax());
+//            $pokemon->setPvMax($pvMax);
+//            $pokemon->setPv(rand(0, $pvMax));
+//            $pokemon->setLevel(rand(0, 100));
+//
+//            $pokemon->setPokedex($pokedexRef);
+//            $pokemons[] = $pokemon;
+//        }
+//
+//        foreach ($pokemons as $pokemon) {
+//           $manager->persist($pokemon);
+//        }
         //POKEMON FIXTURES
-
 
 
    
 
         $manager->flush();
+    }
+
+    /**
+     * Fonction permettant de générer des noms de chimpokomons aléatoirement
+     *
+     * @return string
+     * @throws \Random\RandomException
+     */
+    private function generateChimpokoName(): string
+    {
+        $aPrefixeName = ["meca", "shmi", "ane", "chau", "vey", "ki", "kri", "chini", "ra", "croke", "zipo", "refi"];
+        $aPatternName = ["zouzou", "ro", "ssure", "lsri", "ffoun", "pipo", "poko", "tasdera", "lsi", "rugo", "xyde"];
+        $aSuffixeName = ["mon"];
+
+        $havePrefixe = random_int(0, 1);
+        $haveSuffixe = $havePrefixe ? random_int(0, 1) : 1;
+        $havePatternName = ($havePrefixe && $haveSuffixe) ? random_int(0, 1) : 1;
+
+        $prefixe = $havePrefixe ? $aPrefixeName[array_rand($aPrefixeName)] : "";
+        $patternName = $havePatternName ? $aPatternName[array_rand($aPatternName)] : "";
+        $suffixe = $haveSuffixe ? $aSuffixeName[array_rand($aSuffixeName)] : "";
+
+        return $prefixe . $patternName . $suffixe;
+
     }
 }
